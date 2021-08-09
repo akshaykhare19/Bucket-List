5ad37136a9de19d845eb78926c5e2fd4d78507a0
Akshay Khare <akhare931@gmail.com>
2021-08-09 23:51:29 +0530

    UI Design Completed

---
+++ app/src/main/java/com/project/bucketlist/GroupsActivity.kt
@@ -0,0 +1,11 @@
+package com.project.bucketlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GroupsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_groups)
+    }
+}
